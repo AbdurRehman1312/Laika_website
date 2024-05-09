cf3bfdc4eac239ecd0543dbe56157f26ed0d5b5e
AbdurRehman1312 <abdurrehmansaifi784@gmail.com>
2024-05-10 00:13:29 +0500

    responsiveness

--- src/components/ButtonImage.jsx
+++ src/components/ButtonImage.jsx
@@ -4,7 +4,7 @@ const ButtonImage = ({ style, name, img, Link, imgStyle}) => {
   return (
     <a
       href={Link}
-      className={`bg-dark-gradient py-2 w-[70%] md:w-auto px-3  text-white rounded-[6px] button_hover shadow ${style} text-nowrap flex items-center justify-center`} target="_blank"
+      className={`bg-dark-gradient py-2 w-[100%] md:w-auto px-3  text-white rounded-[6px] button_hover shadow ${style} text-nowrap flex items-center justify-center`} target="_blank"
     >
       <img src={images[img]} alt="" width={35} className={`mr-2 ${imgStyle}`} />
       <span className="md:block">{name}</span>
